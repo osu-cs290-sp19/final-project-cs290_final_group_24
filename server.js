5fd038dc2f8901d841e3c372ff67cc798a1a294b
Haoxiang Dai <dhx@10-249-138-33.wireless.oregonstate.edu>
2019-06-13 02:27:46 -0700

    final

--- server.js
+++ server.js
@@ -23,16 +23,72 @@ sever1.get('/',function (req, res) {
   );
 });
 
-sever1.get('k/:name',function (req, res) {
+sever1.get('/iron_man',function (req, res) {
   var name1 = req.params.name;
-  res.status(200).render('partials/'+name1,data // insert data here
+  res.status(200).render('partials/iron_man',data // insert data here
+  );
+  console.log("data sended");
+});
+sever1.get('/hulk',function (req, res) {
+  var name1 = req.params.name;
+  res.status(200).render('partials/hulk',data // insert data here
+  );
+  console.log("data sended");
+});
+sever1.get('/captain_america',function (req, res) {
+  var name1 = req.params.name;
+  res.status(200).render('partials/captain_america',data // insert data here
   );
   console.log("data sended");
 });
 
+sever1.get('/thor',function (req, res) {
+  var name1 = req.params.name;
+  res.status(200).render('partials/thor',data // insert data here
+  );
+  console.log("data sended");
+});
+
+sever1.get('/Vision',function (req, res) {
+  var name1 = req.params.name;
+  res.status(200).render('partials/Vision',data // insert data here
+  );
+  console.log("data sended");
+});
+
+sever1.get('/Scarlet_witch',function (req, res) {
+  var name1 = req.params.name;
+  res.status(200).render('partials/Scarlet_witch',data // insert data here
+  );
+  console.log("data sended");
+});
+
+sever1.get('/Dr_strange',function (req, res) {
+  var name1 = req.params.name;
+  res.status(200).render('partials/Dr_strange',data // insert data here
+  );
+  console.log("data sended");
+});
+
+sever1.get('/BlackWidow',function (req, res) {
+  var name1 = req.params.name;
+  res.status(200).render('partials/BlackWidow',data // insert data here
+  );
+  console.log("data sended");
+});
+
+sever1.get('/hawkeye',function (req, res) {
+  var name1 = req.params.name;
+  res.status(200).render('partials/hawkeye',data // insert data here
+  );
+  console.log("data sended");
+});
+
+
  sever1.get('*', function (req, res) {
   res.status(404).render('404', {});
 });
+
  
 
 sever1.listen(port);
